077e35633fa8707657aa0d6b9ac93e9fd847d416
Miroslav Palka <Porim@users.noreply.github.com>
2022-06-07 15:17:27 +0100

    fix....

--- spec/bridge-spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'Floating Glass Atlantic Bridge' do
-	it 'is made of glass' do
-		bridge = Bridge.new('glass', [])
-	
-		material = bridge.material
-
-		expect(material).to eq('delicious')
-	end
-end
